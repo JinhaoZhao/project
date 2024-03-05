0307ddb0711f30128250cb6ca2d6138a495638f1
zjh <zjh@yhy.com>
2024-03-05 15:29:46 +0800

    C# study

---
+++ study/.net_core_restful_api/ch18/18.3/FakeXiecheng.API/Controllers/StockController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeXiecheng.API.Dtos;
+using FakeXiecheng.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using System.Text.RegularExpressions;
+using FakeXiecheng.API.ResourceParameters;
+using FakeXiecheng.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using FakeXiecheng.API.Helper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Net.Http.Headers;
+using System.Dynamic;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private ITouristRouteRepository _touristRouteRepository;
+        private readonly IMapper _mapper;
+        private readonly IUrlHelper _urlHelper;
+        private readonly IPropertyMappingService _propertyMappingService;
+
+
+        public StockController(
+            ITouristRouteRepository touristRouteRepository,
+            IMapper mapper,
+            IUrlHelperFactory urlHelperFactory,
+            IActionContextAccessor actionContextAccessor,
+            IPropertyMappingService propertyMappingService
+        )
+        {
+            _touristRouteRepository = touristRouteRepository;
+            _mapper = mapper;
+            _urlHelper = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext);
+            _propertyMappingService = propertyMappingService;
+        }
+
+        [HttpDelete("{touristRouteId}", Name = "DeleteTouristRoute")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetStockList([FromRoute] Guid touristRouteId)
+        {
+            if (!(await _touristRouteRepository.TouristRouteExistsAsync(touristRouteId)))
+            {
+                return NotFound("旅游路线找不到");
+            }
+
+            var touristRoute = await _touristRouteRepository.GetTouristRouteAsync(touristRouteId);
+            _touristRouteRepository.DeleteTouristRoute(touristRoute);
+            await _touristRouteRepository.SaveAsync();
+
+            return NoContent();
+        }
+    }
+}
